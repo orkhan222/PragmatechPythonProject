3247fcd77b0673f050207f9d0e0b5ee83251f721
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2021-06-24 13:47:16 +0400

    Blog hissesi yazildi

--- lahiye_con/pages/urls.py
+++ lahiye_con/pages/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('cart/', views.cart,name="cart"),
     path('order/', views.order,name="order"),
     path('product-detail/', views.productDetail,name="productDetail"),
-    path('men', views.men,name="men"),
-    path('women', views.women,name="women"),
-    path('wishlist', views.wishlist,name="wishlist"),
-    path('checkout', views.checkout,name="checkout"),
+    path('men/', views.men,name="men"),
+    path('women/', views.women,name="women"),
+    path('wishlist/', views.wishlist,name="wishlist"),
+    path('checkout/', views.checkout,name="checkout"),
     
 ]
