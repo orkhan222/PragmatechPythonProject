f78a08efb3c17224c276a52d7ab64a55807faec0
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2021-07-04 15:43:56 +0400

    MyPortfolio problem var idi sildim

---
+++ Projects/Project03/lahiye_con/courses/models.py
@@ -0,0 +1,17 @@
+from typing import DefaultDict
+from django.db import models
+from django.db.models import base
+from django.db.models.fields import TextField
+
+# Create your models here.
+
+class Courses(models.Model):
+    name= models.CharField(max_length=127,unique=True,verbose_name='Kurs adi',help_text='Kurs adini yaz:')
+    description= models.TextField(blank=True,null=True)
+    image=models.ImageField(default='courses/default_course_image.jpg')
+    date=models.DateField(auto_now=True)
+    avaibable= models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return self.name
--- Projects/Project03/lahiye_con/pages/views.py
+++ Projects/Project03/lahiye_con/pages/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def index(request):
    
     return render(request, 'index.html')
+
+
+def about(request):
+   
+    return render(request, 'about.html')
